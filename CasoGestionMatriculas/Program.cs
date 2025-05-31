c5d9aa1664fa9b0fdbf38ccbff92abadf5044552
aaron acuña <aaronacu_85@hotmail.com>
2025-05-31 13:56:08 -0500

    feat: configurated connection.

--- CasoGestionMatriculas/Program.cs
+++ CasoGestionMatriculas/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using CasoGestionMatriculas.Shared.Infrastructure.Persistence.EFC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DataBase Configuration
+
+builder.Services.AddTransient<IDbConnection>(db =>
+
+    new SqlConnection(builder.Configuration
+    .GetConnectionString("CasoGestionMatriculas"))
+);
+builder.Services.AddDbContext<CasoGestionMatriculasContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration
+        .GetConnectionString("CasoGestionMatriculas"));
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -13,6 +33,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(
+    c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()
+);
 
 app.UseAuthorization();
 
